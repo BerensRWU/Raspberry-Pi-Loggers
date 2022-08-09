5f088f8b27ba7e47e6d8f48aca0967ede17aaa5a
BerensRWU <58558898+BerensRWU@users.noreply.github.com>
2022-08-09 15:15:44 +0200

    Update data_logger.py

--- data_logger.py
+++ data_logger.py
@@ -1,77 +1,121 @@
 from sense_hat import SenseHat
-from datetime import datetime
-from csv import writer
+import numpy as np
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from time import sleep, time
 import cv2
+from time import sleep
+from csv import writer
+import pygame
+from pygame.locals import *
+import pyaudio
+import wave
+from gps import *
+from datetime import datetime
 
-def get_sense_data():
-    sense_data = []
-    # Get environmental data
-    sense_data.append(sense.get_temperature())
-    sense_data.append(sense.get_pressure())
-    sense_data.append(sense.get_humidity())
-    # Get orientation data
-    orientation = sense.get_orientation()
-    sense_data.append(orientation["yaw"])
-    sense_data.append(orientation["pitch"])
-    sense_data.append(orientation["roll"])
-    # Get compass data
-    mag = sense.get_compass_raw()
-    sense_data.append(mag["x"])
-    sense_data.append(mag["y"])
-    sense_data.append(mag["z"])
-    # Get accelerometer data
-    acc = sense.get_accelerometer_raw()
-    sense_data.append(acc["x"])
-    sense_data.append(acc["y"])
-    sense_data.append(acc["z"])
-    #Get gyroscope data
-    gyro = sense.get_gyroscope_raw()
-    sense_data.append(gyro["x"])
-    sense_data.append(gyro["y"])
-    sense_data.append(gyro["z"])
-    # Get the date and time
-    sense_data.append(datetime.now())
+import configs
+from utils import get_sense_data, get_GPS_data
 
-    return sense_data
+if os.path.exists(f"{configs.root}/record_meta.txt"):
+    with open(f"{configs.root}/record_meta.txt", "rb") as f:
+        try:
+            f.seek(-2, os.SEEK_END)
+            while f.read(1) != b"\n":
+                f.seek(-2, os.SEEK_CUR)
+        except OSError:
+            f.seek(0)
+        meta_data = f.readline().decode()
+        recording_number = int(meta_data.split(",")[0]) + 1
+        with open(f"{configs.root}/record_meta.txt", "a", newline="") as f:
+            data_writer = writer(f)
+            data_writer.writerow([recording_number, datetime.now()] + configs.sensor_list)
+else:    
+    recording_number = 0
+    
+    with open(f"{configs.root}/record_meta.txt", "a", newline="") as f:
+        data_writer = writer(f)
+        data_writer.writerow([recording_number, datetime.now()] + configs.sensor_list)
 
-sense = SenseHat()
+configs.root = f"{configs.root}/recording_{recording_number}"
+os.mkdir(configs.root)
+os.mkdir(f"{configs.root}/microphone_records")
+os.mkdir(f"{configs.root}/camera_records")
+
+is_running = True
+
+# -- Camera ---
 camera = PiCamera()
-camera.resolution = (860, 540)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(860, 540))
+camera.resolution = configs.camera_resolution
+camera.framerate = configs.camera_framerate
+rawCapture = PiRGBArray(camera, size=configs.camera_resolution)
 sleep(1)
-with open('data.csv', 'w', newline='') as f:
+
+# -- Microphone ---
+audio = pyaudio.PyAudio() # create pyaudio instantiation
+stream = audio.open(format = configs.form_1,rate = configs.samp_rate,channels = configs.chans, \
+                    input_device_index = configs.dev_index, input = True, \
+                    frames_per_buffer = configs.chunk)
+
+# -- GPS ---
+gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+
+with open(f"{configs.root}/gps_records.csv", "w", newline="") as f:
+    data_writer = writer(f)
+    data_writer.writerow(configs.keys_tpv)
+
+# -- SenseHat ---
+sense = SenseHat()
+
+pygame.init()
+pygame.display.set_mode((1, 1))
+
+with open(f"{configs.root}/senseHat_records.csv", "w", newline='') as f:
     data_writer = writer(f)
     
-    data_writer.writerow(['temp', 'pres', 'hum',
-                          'yaw', 'pitch', 'roll',
-                          'mag_x', 'mag_y', 'mag_z',
-                          'acc_x', 'acc_y', 'acc_z',
-                          'gyro_x', 'gyro_y', 'gyro_z',
-                          'datetime'])
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        # grab the raw NumPy array representing the image, then initialize the timestamp
-        # and occupied/unoccupied text
+    data_writer.writerow(["temp", "pres", "hum",
+                          "yaw", "pitch", "roll",
+                          "mag_x", "mag_y", "mag_z",
+                          "acc_x", "acc_y", "acc_z",
+                          "gyro_x", "gyro_y", "gyro_z",
+                          "datetime"])
+
+sense.set_pixels([(255,255,255)]*64)
+
+try:
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):        
+
         image = frame.array
-        # show the frame
-        cv2.imshow("Frame", image)
+        mic_data = stream.read(configs.chunk, exception_on_overflow = False)
+        senseHat_data = get_sense_data(sense)
+        gps_data = get_GPS_data(gpsd)
         
-        data = get_sense_data()
-        data_writer.writerow(data)
-        
-        cv2.imwrite(f"cam_records/{data[-1]}.jpg", image)
-        
-        key = cv2.waitKey(1) & 0xFF
-        # clear the stream in preparation for the next frame
+        with open(f"{configs.root}/senseHat_records.csv", "a", newline="") as f:
+            data_writer = writer(f)
+            data_writer.writerow(senseHat_data)
+
+        cv2.imwrite(f"{configs.root}/camera_records/{senseHat_data[-1]}.jpg", image)
         rawCapture.truncate(0)
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
         
 
-  
-
+        with open(f"{configs.root}/microphone_records/{senseHat_data[-1]}.bit", "wb") as f:
+            f.write(mic_data)
+            
+        if gps_data:
+            with open(f"{configs.root}/gps_records.csv", "a", newline="") as f:        
+                data_writer = writer(f)
+                data_writer.writerow(gps_data + [senseHat_data[-1]])
+                    
+        key_event = pygame.event.get()
+        if key_event:
+            if key_event[0].type in [2,3] :
+                sense.set_pixels([(0,255,0)]*64)
+                is_running = False
+                break       
+                
+finally:
+    if is_running:
+        sense.set_pixels([(255,0,0)]*64)
+    # stop the stream, close it, and terminate the pyaudio instantiation
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
 
